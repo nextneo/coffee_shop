108051bdd6a43bcd0e45d0baa23395b8a7f423b6
ThuyMa <mahongthuy@gmail.com>
2021-08-27 17:57:49 +0700

    add

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        User::create([
+            'name' => 'admin',
+            'last_name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => 'password', // password
+            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);        
+    }
+}
